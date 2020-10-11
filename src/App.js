a966469c2c8b36b907bd7184b4c5b9e74efec3bd
Deiziane Silva <deiziane_ns@hotmail.com>
2020-10-11 16:19:51 -0300

    :zap: adds components files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './App.css';
+import Tetris from './components/Tetris';
+
+const App = (props) => {
+  return <Tetris />;
+};
+
+App.propTypes = {};
+
+export default App;
