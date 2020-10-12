e46de562076646010a9fb27861b987f8f0f82582
Deiziane <deiziane_ns@hotmail.com>
2020-10-11 21:26:49 -0300

    :zap: adds create stage function

---
+++ src/components/Stage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Stage = ({ stage }) => {
+  return <div></div>;
+};
+
+export default Stage;
--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Button from '../Button';
 
-it('renders button correctly', () => {
-  const wrapper = shallow(<Button />);
-  expect(wrapper.exists()).toBeTruthy();
+describe('Button', () => {
+  it('renders button correctly', () => {
+    const wrapper = shallow(<Button />);
+    expect(wrapper.exists()).toBeTruthy();
+  });
 });
