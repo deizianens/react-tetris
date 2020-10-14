923a6cf3b3f763acc42c24960847f0dde5fab9fa
Deiziane <deiziane_ns@hotmail.com>
2020-10-13 22:39:58 -0300

    :sparkles: adds collision detection

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -10,7 +10,7 @@ import {
 
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
-import { createStage } from '../helpers';
+import { createStage, checkCollision } from '../helpers';
 
 const KEYS = {
   LEFT_ARROW: 37,
@@ -23,19 +23,29 @@ const Tetris = () => {
   const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer] = usePlayer();
-  const [stage, setStage] = useStage();
+  const [stage, setStage] = useStage(player, resetPlayer);
 
   const movePlayer = (dir) => {
-    updatePlayerPos({ x: dir, y: 0 });
+    if (!checkCollision(player, stage, { x: dir, y: 0 }))
+      updatePlayerPos({ x: dir, y: 0 });
   };
 
   const startGame = () => {
     setStage(createStage());
     resetPlayer();
+    setGameOver(false);
   };
 
   const drop = () => {
-    updatePlayerPos({ x: 0, y: 1, collided: false });
+    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false });
+    } else {
+      if (player.pos.y < 1) {
+        setGameOver(true);
+        setDropTime(null);
+      }
+      updatePlayerPos({ x: 0, y: 0, collided: true });
+    }
   };
 
   const dropPlayer = () => {
@@ -62,7 +72,7 @@ const Tetris = () => {
               <Display text='Score' />
               <Display text='Rows' />
               <Display text='Level' />
-              <Button onClick={startGame} />
+              <Button callback={startGame} />
             </div>
           )}
         </aside>
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -7,12 +7,12 @@ export const useStage = (player, resetPlayer) => {
 
   useEffect(() => {
     const updateStage = (prevStage) => {
-      const newStage = prevStage.map((row) => {
-        row.map((cell) => (cell[1] === 'clear' ? [0, 'clear'] : cell));
-      });
+      const newStage = prevStage.map((row) =>
+        row.map((cell) => (cell[1] === 'clear' ? [0, 'clear'] : cell))
+      );
 
       //draw the tetromino
-      player.tetromine.forEach((row, y) => {
+      player.tetromino.forEach((row, y) => {
         row.forEach((value, x) => {
           if (value !== 0) {
             newStage[y + player.pos.y][x + player.pos.x] = [
@@ -22,10 +22,16 @@ export const useStage = (player, resetPlayer) => {
           }
         });
       });
+
+      if (player.collided) {
+        resetPlayer();
+      }
+
+      return newStage;
     };
 
     setStage((prev) => updateStage(prev));
-  }, []);
+  }, [player, resetPlayer]);
 
   return [stage, setStage];
 };
